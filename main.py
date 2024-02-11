81545f147f3906e25a81a41d8fa37612a39e7116
watchakorn-18k <wk18k@proton.me>
2024-02-12 00:47:13 +0700

    feat : add cores

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from controller.user import router_user
 from controller.course import router_course
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_user, prefix="/user")
 app.include_router(router_course, prefix="/course")
